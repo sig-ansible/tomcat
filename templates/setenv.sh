3c09b06e90b7bc6f84c03cfe79a518cc4d108412
Daniel Boitnott <dboitnot@gmail.com>
2019-04-25 14:21:14 -0500

    Initial commit

---
+++ templates/setenv.sh
@@ -0,0 +1,4 @@
+JAVA_HOME="{{tomcat_java_home}}"; export JAVA_HOME
+CATALINA_HOME="{{tomcat_root}}"; export CATALINA_HOME
+JAVA_OPTS="{{tomcat_java_opts}}"; export JAVA_OPTS
+CATALINA_OPTS="{{tomcat_catalina_opts}}"; export CATALINA_OPTS
